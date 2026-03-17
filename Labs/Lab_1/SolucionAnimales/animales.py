9b0c521a3890dfdec51a1d6c3a80cd6cce86663c
BAFCS <bryanfabian4@gmail.com>
2026-03-17 12:06:12 -0300

    reformateo ruff de SolucionAnimales

--- Labs/Lab_1/SolucionAnimales/animales.py
+++ Labs/Lab_1/SolucionAnimales/animales.py
@@ -1,12 +1,12 @@
 def animales(input):
-    '''
+    """
     Como hace el animalito?
-    '''
-    if input.lower() == 'gato':
-        print('miau')
-    elif input.lower() == 'perro': #Se modificó, cambiando if por elif
-        print('guau')
-    elif input.lower() == 'fox': #Se modificó, cambiando if por elif
-        print('https://www.youtube.com/watch?v=jofNR_WkoCE')
+    """
+    if input.lower() == "gato":
+        print("miau")
+    elif input.lower() == "perro":  # Se modificó, cambiando if por elif
+        print("guau")
+    elif input.lower() == "fox":  # Se modificó, cambiando if por elif
+        print("https://www.youtube.com/watch?v=jofNR_WkoCE")
     else:
-        raise ValueError('animal no reconocido :(')
+        raise ValueError("animal no reconocido :(")
