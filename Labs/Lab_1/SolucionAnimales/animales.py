4e4eda5ececefecddc23229e4d32df182c508cc0
BAFCS <bryanfabian4@gmail.com>
2026-03-16 22:04:23 -0300

    Lab1: Se agrega la solución del repositorio de animales en la carpeta SolucionAnimales

---
+++ Labs/Lab_1/SolucionAnimales/animales.py
@@ -0,0 +1,14 @@
+def animales(input):
+    
+    '''
+    Como hace el animalito?
+    '''
+    
+    if input.lower() == 'gato':
+        print('miau')
+    elif input.lower() == 'perro': #Se modificó, cambiando if por elif
+        print('guau')
+    elif input.lower() == 'fox': #Se modificó, cambiando if por elif
+        print('https://www.youtube.com/watch?v=jofNR_WkoCE')
+    else:
+        raise ValueError('animal no reconocido :(')
