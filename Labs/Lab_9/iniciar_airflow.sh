38a8f08c5dd50075ac681a2d09ed406c29ff83dd
BAFCS <bryanfabian4@gmail.com>
2026-06-21 16:47:03 -0400

    Lab9: carga de datos para la implementación de Dag

---
+++ Labs/Lab_9/iniciar_airflow.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Configurar la carpeta actual como base de Airflow
+export AIRFLOW_HOME=$(pwd)
+
+echo "Inicializando la base de datos..."
+airflow db migrate
+
+# Ver la contraseña. Si no se en un comienzo, ejecutar airflow standalone, parar el proceso y luego ejecutar nuevamente este comando. 
+cat $AIRFLOW_HOME/simple_auth_manager_passwords.json.generated 
+
+# # levanta scheduler + webserver en http://localhost:8080
+echo "Levantando Airflow Standalone en http://localhost:8080..."
+airflow standalone
